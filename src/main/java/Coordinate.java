cf72f1b67e4d69a2cf600d2c2302010206b62bf1
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-07-08 15:08:58 +0100

    generate constructor

--- src/main/java/Coordinate.java
+++ src/main/java/Coordinate.java
@@ -2,4 +2,13 @@ public class Coordinate {
     public static final int MAX_FREQUENCY = 1440;
     public static final int MIN_FREQUENCY = 5;
 
+    private String startingTimestamp;
+    private String endingTimestamp;
+    private int frequency;
+
+    public Coordinate(String startingTimestamp, String endingTimestamp, int frequency) {
+        this.startingTimestamp = startingTimestamp;
+        this.endingTimestamp = endingTimestamp;
+        this.frequency = frequency;
+    }
 }
