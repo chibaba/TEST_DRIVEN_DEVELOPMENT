abe20dc577bd1a0386e5fa0d480a87bac70f93a5
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-07-08 15:14:20 +0100

    setters and getters initialized

--- src/main/java/Coordinate.java
+++ src/main/java/Coordinate.java
@@ -6,6 +6,30 @@ public class Coordinate {
     private String endingTimestamp;
     private int frequency;
 
+    public String getStartingTimestamp() {
+        return startingTimestamp;
+    }
+
+    public void setStartingTimestamp(String startingTimestamp) {
+        this.startingTimestamp = startingTimestamp;
+    }
+
+    public String getEndingTimestamp() {
+        return endingTimestamp;
+    }
+
+    public void setEndingTimestamp(String endingTimestamp) {
+        this.endingTimestamp = endingTimestamp;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+
     public Coordinate(String startingTimestamp, String endingTimestamp, int frequency) {
         this.startingTimestamp = startingTimestamp;
         this.endingTimestamp = endingTimestamp;
