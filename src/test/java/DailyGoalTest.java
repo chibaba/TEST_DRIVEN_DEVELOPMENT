04619a97619baa7c09dc40049414dff15cb5a106
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-07-08 23:56:21 +0100

    writing a parametized test

--- src/test/java/DailyGoalTest.java
+++ src/test/java/DailyGoalTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class DailyGoalTest {
@@ -9,5 +10,29 @@ public class DailyGoalTest {
         DailyGoal dailyGoal = new DailyGoal(DailyGoal.DAILY_GOAL);
         Assertions.assertFalse(dailyGoal.hasMetGoal(steps));
 
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 10, 9999 })
+    public void testNotMetStep(int steps) {
+        DailyGoal dailyGoal = new DailyGoal(DailyGoal.DAILY_GOAL);
+        Assertions.assertFalse(dailyGoal.hasMetGoal(steps));
+    }
+    @ParameterizedTest
+    @CsvSource({
+            "10,     false",
+            "9999,   false",
+            "10000,  true",
+            "20000,  true"
+    })
+    public void testHasMetStepGoal(int steps, boolean expected) {
+        DailyGoal dailyGoal = new DailyGoal(DailyGoal.DAILY_GOAL);
+        //Using a lambda will lazily evaluate the expression
+        Assertions.assertTrue(
+                dailyGoal.hasMetGoal(steps) == expected,
+                () -> "With " + steps +
+                        " steps, hasMetGoal() should return  " +
+                        expected
+        );
     }
 }
