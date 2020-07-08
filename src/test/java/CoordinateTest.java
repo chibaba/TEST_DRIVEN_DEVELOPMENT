63131a34d68eacd92928d2742917b7baa32099e4
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-07-08 16:53:46 +0100

    cchecking if date is properly formatted

--- src/test/java/CoordinateTest.java
+++ src/test/java/CoordinateTest.java
@@ -2,6 +2,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Date;
+
 public class CoordinateTest  {
 
     @Test
@@ -12,6 +14,25 @@ public class CoordinateTest  {
                 "2020-12-30T05:15Z",
                 60
         );
-        Assertions.assertTrue(schedule.getFrequency() < 50);
+        int frequency = schedule.getFrequency();
+        Assertions.assertTrue(frequency
+        < Coordinate.MAX_FREQUENCY);
+        Assertions.assertTrue(frequency
+        > Coordinate.MIN_FREQUENCY);
+
     }
+    @Test
+    @DisplayName("Timestamp will be null if not formatted correctly")
+    void testStartingTimestamps() {
+        Coordinate schedule = new Coordinate(
+                "2020/12/15T15:32Z",
+                "20202-12-15T15:35Z",
+                60
+        );
+        Date starting = schedule.getStartingTimestampAsDate();
+        //Timestamp is not formatted properly.
+        Assertions.assertNull(starting);
+    }
+    @Test
+
 }
