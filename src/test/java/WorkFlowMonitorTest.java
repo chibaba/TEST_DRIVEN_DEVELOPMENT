5e3d080df2912e3c79361877f5852d22262c0459
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-07-09 09:06:29 +0100

    Using mocks when testing

--- src/test/java/WorkFlowMonitorTest.java
+++ src/test/java/WorkFlowMonitorTest.java
@@ -1,10 +1,13 @@
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import workflow.EmailNotifier;
 import workflow.WorkFlowClient;
 import workflow.WorkFlowMonitor;
+import workflow.WorkFlowStatus;
 
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
+
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 
 public class WorkFlowMonitorTest {
@@ -17,6 +20,22 @@ public class WorkFlowMonitorTest {
         emailNotifier = mock(EmailNotifier.class);
         workflowClient = mock(WorkFlowClient.class);
         workflowMonitor = new WorkFlowMonitor(emailNotifier, workflowClient);
+    }
+    @Test
+    public void testSuccess() {
+        String id = "WORKFLOW-1";
+        WorkFlowStatus workflowStatus = new WorkFlowStatus(id, WorkFlowStatus.OK);
+        when(workflowClient.getStatus(id)).thenReturn(workflowStatus);
+        workflowMonitor.checkStatus(id);
+        verify(emailNotifier, times(0)).sendFailureEmail(workflowStatus);
+    }
+    @Test
+    public void testFailure() {
+        String id = "WORKFLOW-1";
+        WorkFlowStatus workflowStatus = new WorkFlowStatus(id, WorkFlowStatus.ERROR);
+        when(workflowClient.getStatus(anyString())).thenReturn(workflowStatus);
+        workflowMonitor.checkStatus(id);
+        verify(emailNotifier).sendFailureEmail(workflowStatus);
     }
 
 }
