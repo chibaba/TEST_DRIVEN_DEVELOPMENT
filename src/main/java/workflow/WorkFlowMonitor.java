c09fddd0282ef03b6c0224508d6f820a394895cf
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-07-09 08:49:32 +0100

    using mocks to test deep workflow

--- src/main/java/workflow/WorkFlowMonitor.java
+++ src/main/java/workflow/WorkFlowMonitor.java
@@ -3,4 +3,17 @@ package workflow;
 public class WorkFlowMonitor {
     private EmailNotifier emailNotifier;
     private WorkFlowClient workflowClient;
+
+    public WorkFlowMonitor(EmailNotifier emailNotifier, WorkFlowClient workflowClient) {
+        this.emailNotifier = emailNotifier;
+        this.workflowClient = workflowClient;
+    }
+    public void checkStatus(String id) {
+        WorkFlowStatus workflowStatus = workflowClient.getStatus(id);
+        if (!workflowStatus.isOk()) {
+            emailNotifier.sendFailureEmail(workflowStatus);
+        }
+    }
+
+
 }
