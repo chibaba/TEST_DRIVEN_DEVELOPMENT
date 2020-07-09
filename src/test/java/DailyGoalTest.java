215a99d613fa925458fdae72dd81971b8786e9f1
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-07-09 02:19:12 +0100

    cleaning up

--- src/test/java/DailyGoalTest.java
+++ src/test/java/DailyGoalTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class DailyGoalTest {
     @ParameterizedTest
-    @ValueSource(ints = { 1000, 11000})
+    @ValueSource(ints = { 10000, 11000})
     public void testMetStepGoal(int steps) {
         DailyGoal dailyGoal = new DailyGoal(DailyGoal.DAILY_GOAL);
         Assertions.assertFalse(dailyGoal.hasMetGoal(steps));
